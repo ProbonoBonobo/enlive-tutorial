a39b5f689e9c9fdf8e01bf28da86d5f37136adbe
Kevin Zeidler <kzeidler@gmail.com>
2017-05-18 23:40:31 -0700

    updated broken nytimes link (http -> https)

--- src/tutorial/scrape3.clj
+++ src/tutorial/scrape3.clj
@@ -2,7 +2,7 @@
   (:require [net.cgrand.enlive-html :as html]
             [clojure.string :as str]))
 
-(def ^:dynamic *base-url* "http://nytimes.com/")
+(def ^:dynamic *base-url* "https://www.nytimes.com")
 
 (def ^:dynamic *story-selector*
      [[:article.story
